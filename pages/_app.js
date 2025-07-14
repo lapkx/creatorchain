08b490d0a527344a256cc80c5369b60ee1ad2a49
solaceglobal123 <Stryng.dev@gmail.com>
2025-07-14 10:25:30 -0500

    vercel speed/analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import "../styles/globals.css";
 import { AuthProvider } from "../context/AuthContext";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }) {
   return (
